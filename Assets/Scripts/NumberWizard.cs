9c989371067e1fa72bf6fde7ea424bb3e46399ae
Pablo C. Leite <pablo.leite@gmail.com>
2017-02-18 22:10:11 +0000

    Adding game logic and performing some refactoring.

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -4,29 +4,54 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
+    private const int MaxNumber = 1000;
+    private const int MinMumber = 1;
+
+    int max;
+    int min;
+    int guess = 500;
+
     // Use this for initialization
     void Start() {
-        print("Welcome to number wizard");
-        print("Pick a number in your head, but don't tell me.");
-
-        int max = 1000;
-        int min = 1;
-
-        print("The highest number you can pick is " + max);
-        print("The lowest number you can pick is " + min);
-
-        print("Is the number higher or lower than 500?");
-        print("Up = higher, down = lower, return = equals.");
+        StartGame();
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            print("Up arrow pressed");
+            min = guess;
+            NextGuess();
         } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            print("Down arrow pressed");
+            max = guess;
+            NextGuess();
         } else if (Input.GetKeyDown(KeyCode.Return)) {
-            print("Return pressed");
+            print("I won!");
+            StartGame();
         }
+    }
+
+    private void StartGame() {
+        max = MaxNumber;
+        min = MinMumber;
+        guess = (max + min) / 2;
+
+        print("========================");
+        print("Welcome to number wizard");
+        print("Pick a number in your head, but don't tell me.");
+
+        print("The highest number you can pick is " + max);
+        print("The lowest number you can pick is " + min);
+
+        print("Is the number higher or lower than " + guess + "?");
+        print("Up = higher, down = lower, return = equals.");
+
+        //Adding 1 so that we can pick the MaxNumber number.
+        max += 1;
+    }
+
+    private void NextGuess() {
+        guess = (max + min) / 2;
+        print("Higher or lower than " + guess + "?");
+        print("Up = higher, down = lower, return = equals.");
     }
 }
