d4fc4e17937cc0c7c627c5da5cd5fc20857f7dc4
Pablo C. Leite <pablo.leite@gmail.com>
2017-02-21 00:54:16 +0000

    Renaming local variables, so they make more sense now.

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -7,9 +7,9 @@ public class NumberWizard : MonoBehaviour {
     private const int MaxNumber = 1000;
     private const int MinMumber = 1;
 
-    int max;
-    int min;
-    int guess = 500;
+    int currentMax;
+    int currentMin;
+    int guess;
 
     // Use this for initialization
     void Start() {
@@ -19,10 +19,10 @@ public class NumberWizard : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            min = guess;
+            currentMin = guess;
             NextGuess();
         } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            max = guess;
+            currentMax = guess;
             NextGuess();
         } else if (Input.GetKeyDown(KeyCode.Return)) {
             print("I won!");
@@ -31,26 +31,26 @@ public class NumberWizard : MonoBehaviour {
     }
 
     private void StartGame() {
-        max = MaxNumber;
-        min = MinMumber;
-        guess = (max + min) / 2;
+        currentMax = MaxNumber;
+        currentMin = MinMumber;
+        guess = (currentMax + currentMin) / 2;
 
         print("========================");
         print("Welcome to number wizard");
         print("Pick a number in your head, but don't tell me.");
 
-        print("The highest number you can pick is " + max);
-        print("The lowest number you can pick is " + min);
+        print("The highest number you can pick is " + currentMax);
+        print("The lowest number you can pick is " + currentMin);
 
         print("Is the number higher or lower than " + guess + "?");
         print("Up = higher, down = lower, return = equals.");
 
         //Adding 1 so that we can pick the MaxNumber number.
-        max += 1;
+        currentMax += 1;
     }
 
     private void NextGuess() {
-        guess = (max + min) / 2;
+        guess = (currentMax + currentMin) / 2;
         print("Higher or lower than " + guess + "?");
         print("Up = higher, down = lower, return = equals.");
     }
